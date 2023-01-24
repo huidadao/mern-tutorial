d79b3800378bcbe16f977ab01b9642c8630143eb
huidadao <huidadao@gmail.com>
2023-01-24 09:46:45 +0900

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorHandler')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5001
 
-const app = express()
+connectDB()
 
+const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
